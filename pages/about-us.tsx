7bd718c5105bc731db53e51603442203d1593250
Esteban <estebanesteban-escobar1111@hotmail.com>
2023-04-22 21:04:52 -0500

    first commit - created simple project structure and built the dummy pages

---
+++ pages/about-us.tsx
@@ -0,0 +1,14 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>About us page</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eveniet porro molestias reprehenderit atque placeat blanditiis ipsum iure optio! At, pariatur nam autem atque delectus animi reprehenderit nulla voluptatem quasi?
+            Suscipit ipsum qui, dolorem ipsa officia magni quam obcaecati vel esse architecto et nobis cumque sint inventore ratione, voluptatem aspernatur iste consequuntur quod labore, illum optio fugiat! Laborum, suscipit iusto.
+
+            </p>
+        </div>
+    )
+}
+
+export default Page
---
+++ pages/events/index.tsx
@@ -0,0 +1,26 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+
+      <div>
+        <a href="">
+          <img />
+          <h2>Events in San Francisco</h2>
+        </a>
+
+        <a href="">
+          <img />
+          <h2>Events in london</h2>
+        </a>
+
+        <a href="">
+          <img />
+          <h2>Events in Barcelona</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
