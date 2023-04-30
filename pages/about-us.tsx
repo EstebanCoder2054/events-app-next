e5768c4ae444ea0a5e3d23a37cd512a70092d8e4
Esteban <estebanesteban-escobar1111@hotmail.com>
2023-04-30 15:14:34 -0500

    added data fetching and dinamic routes

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,14 +1,18 @@
-const Page = () => {
-    return (
-        <div>
-            <h1>About us page</h1>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eveniet porro molestias reprehenderit atque placeat blanditiis ipsum iure optio! At, pariatur nam autem atque delectus animi reprehenderit nulla voluptatem quasi?
-            Suscipit ipsum qui, dolorem ipsa officia magni quam obcaecati vel esse architecto et nobis cumque sint inventore ratione, voluptatem aspernatur iste consequuntur quod labore, illum optio fugiat! Laborum, suscipit iusto.
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About us page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eveniet
+        porro molestias reprehenderit atque placeat blanditiis ipsum iure optio!
+        At, pariatur nam autem atque delectus animi reprehenderit nulla
+        voluptatem quasi? Suscipit ipsum qui, dolorem ipsa officia magni quam
+        obcaecati vel esse architecto et nobis cumque sint inventore ratione,
+        voluptatem aspernatur iste consequuntur quod labore, illum optio fugiat!
+        Laborum, suscipit iusto.
+      </p>
+    </div>
+  );
+};
 
-            </p>
-        </div>
-    )
-}
-
-export default Page
+export default AboutUsPage;
---
+++ pages/events/[cat]/[id].tsx
@@ -0,0 +1,5 @@
+const EventPage = () => {
+  return <h1>A simple dinamic page</h1>;
+};
+
+export default EventPage;
